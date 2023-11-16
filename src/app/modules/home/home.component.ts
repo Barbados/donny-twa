5591611ad67ad51fb69d80c7ca9af4860fd9e71e
vlacomov <vlacomov@gmail.com>
2023-11-16 22:01:27 +0300

    1. Added ng-zorro components 2. added experemental component with list

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import WebApp from '@twa-dev/sdk';
+import { PatientsListComponent } from '../patients/patients-list/patients-list.component';
 
 @Component({
   selector: 'donny-twa-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PatientsListComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   showTgAlert() {
-    WebApp.showAlert('Hey!')
+    WebApp.showAlert('Hey!');
   }
 }
